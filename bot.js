9e1d08644c5aae74ad0c4d2411bc31b8be7e778d
KrypTonbot <45620388+KrypTonbot@users.noreply.github.com>
2018-12-22 11:44:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,18 +3,10 @@ const client = new Discord.Client();
 client.on('message', message => {
  var prefix = "$";
 client.on('ready', () => {
+ });
   console.log(`Logged in as ${client.user.tag}!`);
-
-client.on('message', message => {
-              if (!message.channel.guild) return;
-      if(message.content =='-count')
-      var IzRo = new Discord.RichEmbed()
-      .setThumbnail(message.author.avatarURL)
-      .setFooter(message.author.username, message.author.avatarURL)
-      .setTitle('🌷| Members info')
-      .addBlankField(true)
-      .addField('عدد اعضاء السيرفر',`${message.guild.memberCount}`)
-      message.channel.send(IzRo);
-    });
-
+client.on("ready", () => { 
+  function lol() {
+    client.guilds.get('434062859715084289').roles.find("name", "Owner").setColor("RANDOM");
+   });
 client.login(process.env.BOT_TOKEN);
