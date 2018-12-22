ead7567b1e27fe9996c2a8ea2702a5641acb6068
KrypTonbot <45620388+KrypTonbot@users.noreply.github.com>
2018-12-22 11:22:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+client.on('message', message => {
+ var prefix = "$";
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -10,5 +11,20 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 });
-
-client.login('NTIzNzM5ODY4MzI5NjcyNzIy.Dv-ANw.2vwGQ10MM9vgXuT5gWcJq_qbdNY');
+client.on('message', message => {
+    if (message.content.startsWith("&avatar")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+client.login(process.env.BOT_TOKEN);
